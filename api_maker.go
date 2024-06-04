f895a03bfdcd8ea26fb00b115bb849991ac98b3b
YasinSaee <yasinvsaee@gmail.com>
2024-06-04 20:45:30 +0330

    add comment

--- api_maker.go
+++ api_maker.go
@@ -1,3 +1,7 @@
+// Package api defines structures and methods for handling API services with
+// functionalities such as creating and editing resources, along with handling
+// authentication, authorization, and validation.
+
 package apimaker
 
 import (
@@ -8,6 +12,8 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// APIService defines the structure for an API service, containing necessary
+// components such as name, group, validator, and logger.
 type APIService struct {
 	Name      string
 	Group     *echo.Group
@@ -15,6 +21,16 @@ type APIService struct {
 	Logger    echo.Logger
 }
 
+// NewAPIService creates a new instance of APIService with the given parameters.
+//
+// Parameters:
+// - name: The name of the API service.
+// - group: The echo group associated with the API service.
+// - validator: The validator to be used for request validation.
+// - logger: The logger to be used for logging.
+//
+// Returns:
+// - *APIService: A pointer to the newly created APIService instance.
 func NewAPIService(name string, group *echo.Group, validator echo.Validator, logger echo.Logger) *APIService {
 	return &APIService{
 		Name:      name,
@@ -24,10 +40,13 @@ func NewAPIService(name string, group *echo.Group, validator echo.Validator, log
 	}
 }
 
+// ServiceType defines the structure for a service type, containing its name.
 type ServiceType struct {
 	Name string
 }
 
+// ServiceRequest defines the structure for a service request, containing
+// context, model, form, after save hook, before save hook, and security.
 type ServiceRequest struct {
 	Context    echo.Context
 	Model      Model
@@ -37,6 +56,14 @@ type ServiceRequest struct {
 	Security   Security
 }
 
+// RequestService handles a service request based on the provided service type.
+//
+// Parameters:
+// - serviceType: The type of service request (e.g., CreateServiceRequest, UpdateServiceRequest).
+// - service: The service request containing context, model, form, hooks, and security.
+//
+// Returns:
+// - error: An error if the service type is not recognized or if the service request fails.
 func (a APIService) RequestService(serviceType string, service ServiceRequest) error {
 	if serviceType == CreateServiceRequest {
 		return service.Create(a)
@@ -46,23 +73,42 @@ func (a APIService) RequestService(serviceType string, service ServiceRequest) e
 	return errors.New("please select a service type")
 }
 
+// Create handles the creation of a new resource in the API service.
+// It performs the following steps:
+// 1. Authentication: If an authenticator is provided, it checks if the request is authenticated.
+// 2. Authorization: If an authorizer is provided, it checks if the request is authorized.
+// 3. Data Binding: It binds the request data to the provided model.
+// 4. Before Save Hook: It calls an optional before save function to perform any pre-save operations.
+// 5. Save: It saves the model to the database.
+// 6. After Save Hook: It calls an optional after save function to perform any post-save operations.
+// 7. Success Response: It returns a success response if all steps are completed without errors.
+//
+// Parameters:
+// - createService: A ServiceRequest struct containing the context, security handlers, form, model, and hooks.
+// - a: An APIService interface providing methods for error and success responses.
+//
+// Returns:
+// - error: An error if any step fails; otherwise, nil.
 func (createService ServiceRequest) Create(a APIService) error {
 	var (
 		err error
 	)
 
+	// Step 1: Authentication
 	if createService.Security.Authenticator != nil {
 		if authenticated, err := createService.Security.Authenticator(createService.Context); err != nil || !authenticated {
 			return a.ErrorResponse(createService.Context, http.StatusUnauthorized, err, "authentication failed")
 		}
 	}
 
+	// Step 2: Authorization
 	if createService.Security.Authorizer != nil {
 		if authorized, err := createService.Security.Authorizer(createService.Context); err != nil || !authorized {
 			return a.ErrorResponse(createService.Context, http.StatusForbidden, err, "authorization failed")
 		}
 	}
 
+	// Step 3: Data Binding
 	if err = BindStruct(createService.Context, createService.Form, createService.Model); err != nil {
 		return a.ErrorResponse(createService.Context, http.StatusBadRequest, err, "failed to bind form")
 	}
@@ -71,22 +117,26 @@ func (createService ServiceRequest) Create(a APIService) error {
 		return a.ErrorResponse(createService.Context, http.StatusBadRequest, err, "failed to bind form data")
 	}
 
+	// Step 4: Before Save Hook
 	if createService.BeforeSave.Function != nil {
 		if err = createService.BeforeSave.Function(createService.Model, createService.BeforeSave.Params...); err != nil {
 			return a.ErrorResponse(createService.Context, http.StatusBadRequest, err, fmt.Sprintf("cannot use function beforesave, error : %s ", err.Error()))
 		}
 	}
 
+	// Step 5: Save the Model
 	if err = createService.Model.Save(); err != nil {
 		return a.ErrorResponse(createService.Context, http.StatusInternalServerError, err, fmt.Sprintf("cannot add %s", a.Name))
 	}
 
+	// Step 6: After Save Hook
 	if createService.AfterSave.Function != nil {
 		if err = createService.AfterSave.Function(createService.Model, createService.AfterSave.Params...); err != nil {
 			return a.ErrorResponse(createService.Context, http.StatusBadRequest, err, fmt.Sprintf("cannot use function aftersave, error : %s ", err.Error()))
 		}
 	}
 
+	// Step 7: Success Response
 	return SuccessResponse(
 		createService.Context,
 		http.StatusOK,
@@ -96,29 +146,52 @@ func (createService ServiceRequest) Create(a APIService) error {
 	)
 }
 
+// Edit handles the editing of an existing resource in the API service.
+// It performs the following steps:
+// 1. Extract ID: Retrieves the ID of the resource to be edited from the context parameters.
+// 2. Authentication: If an authenticator is provided, it checks if the request is authenticated.
+// 3. Authorization: If an authorizer is provided, it checks if the request is authorized.
+// 4. Fetch Resource: Retrieves the existing resource by its ID.
+// 5. Data Binding: It binds the request data to the fetched model.
+// 6. Before Save Hook: It calls an optional before save function to perform any pre-save operations.
+// 7. Save: It updates the model in the database.
+// 8. After Save Hook: It calls an optional after save function to perform any post-save operations.
+// 9. Success Response: It returns a success response if all steps are completed without errors.
+//
+// Parameters:
+// - updateService: A ServiceRequest struct containing the context, security handlers, form, model, and hooks.
+// - a: An APIService interface providing methods for error and success responses.
+//
+// Returns:
+// - error: An error if any step fails; otherwise, nil.
 func (updateService ServiceRequest) Edit(a APIService) error {
 	var (
 		err error
 	)
 
+	// Step 1: Extract ID
 	id := updateService.Context.Param("id")
 
+	// Step 2: Authentication
 	if updateService.Security.Authenticator != nil {
 		if authenticated, err := updateService.Security.Authenticator(updateService.Context); err != nil || !authenticated {
 			return a.ErrorResponse(updateService.Context, http.StatusUnauthorized, err, "authentication failed")
 		}
 	}
 
+	// Step 3: Authorization
 	if updateService.Security.Authorizer != nil {
 		if authorized, err := updateService.Security.Authorizer(updateService.Context); err != nil || !authorized {
 			return a.ErrorResponse(updateService.Context, http.StatusForbidden, err, "authorization failed")
 		}
 	}
 
+	// Step 4: Fetch Resource
 	if err = updateService.Model.GetOne(id); err != nil {
 		return a.ErrorResponse(updateService.Context, http.StatusBadRequest, err, fmt.Sprintf("cannot find any %s", a.Name))
 	}
 
+	// Step 5: Data Binding
 	if err = BindStruct(updateService.Context, updateService.Form, updateService.Model); err != nil {
 		return a.ErrorResponse(updateService.Context, http.StatusBadRequest, err, "failed to bind form")
 	}
@@ -127,22 +200,26 @@ func (updateService ServiceRequest) Edit(a APIService) error {
 		return a.ErrorResponse(updateService.Context, http.StatusBadRequest, err, "failed to bind form data")
 	}
 
+	// Step 6: Before Save Hook
 	if updateService.BeforeSave.Function != nil {
 		if err = updateService.BeforeSave.Function(updateService.Model, updateService.BeforeSave.Params...); err != nil {
 			return a.ErrorResponse(updateService.Context, http.StatusBadRequest, err, fmt.Sprintf("cannot use function beforesave, error : %s ", err.Error()))
 		}
 	}
 
+	// Step 7: Save the Model
 	if err = updateService.Model.Save(); err != nil {
 		return a.ErrorResponse(updateService.Context, http.StatusInternalServerError, err, fmt.Sprintf("cannot edit %s", a.Name))
 	}
 
+	// Step 8: After Save Hook
 	if updateService.AfterSave.Function != nil {
 		if err = updateService.AfterSave.Function(updateService.Model, updateService.AfterSave.Params...); err != nil {
 			return a.ErrorResponse(updateService.Context, http.StatusBadRequest, err, fmt.Sprintf("cannot use function aftersave, error : %s ", err.Error()))
 		}
 	}
 
+	// Step 9: Success Response
 	return SuccessResponse(updateService.Context, http.StatusOK, fmt.Sprintf("successfully edited %s", a.Name), echo.Map{a.Name: updateService.Model}, MetaData{})
 }
 
